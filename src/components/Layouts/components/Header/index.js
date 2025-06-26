df8b607259c50c052ff99a867b0e9410e0aa29ee
HoanNam <m8mi0909@gmail.com>
2025-06-26 15:09:40 +0700

    Xây dựng UI phần Header #1

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,90 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartArrowDown, faHeartCircleCheck, faUser } from '@fortawesome/free-solid-svg-icons';
+
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <nav className={classNames('navbar', 'navbar-expand-lg', 'navbar-light', 'bg-light', cx('wrapper'))}>
+      <div className="container-fluid">
+        <a className={classNames('navbar-brand', 'fw-bold', cx('navbar-logo'))} href="#">
+          SHOP.CO
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={classNames('collapse', 'navbar-collapse', cx('navbar-content'))} id="navbarSupportedContent">
+          {/* Nav list */}
+          <ul className={classNames('navbar-nav', 'mb-2', 'mb-lg-0', 'fs-4', cx('navbar-list'))}>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Shop
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                On sale
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                New Arrivals
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Top selling
+              </a>
+            </li>
+          </ul>
+
+          {/* Search */}
+          <div className={cx('search')}>
+            <input placeholder="Search new products..." />
+            <button className={cx('search-btn')}>
+              <svg
+                width="2.4rem"
+                height="2.4rem"
+                fill="currentColor"
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M22 10C15.3726 10 10 15.3726 10 22C10 28.6274 15.3726 34 22 34C28.6274 34 34 28.6274 34 22C34 15.3726 28.6274 10 22 10ZM6 22C6 13.1634 13.1634 6 22 6C30.8366 6 38 13.1634 38 22C38 25.6974 36.7458 29.1019 34.6397 31.8113L43.3809 40.5565C43.7712 40.947 43.7712 41.5801 43.3807 41.9705L41.9665 43.3847C41.5759 43.7753 40.9426 43.7752 40.5521 43.3846L31.8113 34.6397C29.1019 36.7458 25.6974 38 22 38C13.1634 38 6 30.8366 6 22Z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+
+          {/* Actions */}
+          <div className={classNames('d-flex', 'gap-3', cx('actions'))}>
+            <button className={cx('action-btn')}>
+              <FontAwesomeIcon className={cx('action-icon')} icon={faHeartCircleCheck} />
+            </button>
+            <button className={cx('action-btn')}>
+              <FontAwesomeIcon className={cx('action-icon')} icon={faCartArrowDown} />
+            </button>
+            <button className={cx('action-btn')}>
+              <FontAwesomeIcon className={cx('action-icon')} icon={faUser} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
